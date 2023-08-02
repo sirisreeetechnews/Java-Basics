cfe1b2fdb18e49a23aef9cf64a1bdec92eaeb915
sirisreeetechnews <sirisreetechnews@gmail.com>
2023-08-02 22:01:12 +0530

    missing code commit

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,5 +1,12 @@
 public class ArrayDemo {
   public static void main(String[] args) {
-
+    // Array Declaration & initialization
+    String[] cars = {"Volvo", "BMW", "Ford"};
+    // print first element (0th index)
+    System.out.println("car = " + cars[0]);
+    //update first element
+    cars[0] = "Audi";
+    System.out.println("updated car="+cars[0]);
+    System.out.println("Array Length="+cars.length);
   }
 }
--- src/Division.java
+++ src/Division.java
@@ -1,5 +1,10 @@
 public class Division {
     public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        int div;
 
+        div = a/b;
+        System.out.println("a/b = " + div);
     }
 }
