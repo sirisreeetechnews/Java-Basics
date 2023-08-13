aab1a987d64458957afac5b5bde67da95d511eec
sirisreeetechnews <sirisreetechnews@gmail.com>
2023-08-13 08:47:51 +0530

    class and object

---
+++ src/Dog.java
@@ -0,0 +1,22 @@
+  public class Dog {
+    String breed;
+    String size;
+    int age;
+    String color;
+
+    public void eat() {
+      System.out.println("eating.");
+    }
+
+    public void sleep() {
+      System.out.println("sleeping.");
+    }
+
+    public void sit() {
+      System.out.println("sitting.");
+    }
+
+    public void run() {
+      System.out.println("running.");
+    }
+  }
---
+++ src/HowToCreateObject.java
@@ -0,0 +1,16 @@
+public class HowToCreateObject {
+ public static void main(String[] args) {
+  // we can use new keyword to create Object
+  Dog dogObject = new Dog();
+  dogObject.breed = "Nepolian 1";
+  dogObject.size="large";
+  dogObject.age = 2;
+  dogObject.color = "Black";
+
+  Dog dogObject2 = new Dog();
+  dogObject2.breed = "chow chow";
+  dogObject2.size="small";
+  dogObject2.age = 1;
+  dogObject2.color = "white";
+ }
+}
